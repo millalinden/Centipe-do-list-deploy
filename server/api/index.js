6166961217e006f6e27d2abb6fe936027c51ed4f
millalinden <milla.linden@hyperisland.se>
2024-04-07 10:34:57 +0200

    test

--- server/api/index.js
+++ server/api/index.js
@@ -6,11 +6,10 @@ const PORT = process.env.PORT || 3000;
 async function main() {
   const app = express();
 
+  // Instead of setupRouting being a property of an object, it's now a function directly exported.
   setupRouting(app);
 
   app.listen(PORT, () => console.log(`Listening on port ${PORT}....`));
 }
 
 main();
-
-module.exports = app;
